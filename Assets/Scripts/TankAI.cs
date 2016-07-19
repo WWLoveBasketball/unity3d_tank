19c0186dd33579e43178c790ecc8936006fbef92
e <wangwweeii@sina.com>
2016-07-19 23:40:39 +0800

    add tank health & tank explosion & tank AI

--- Assets/Scripts/TankAI.cs
+++ Assets/Scripts/TankAI.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TankAI : MonoBehaviour {
+public class TankAI : unityClass {
 
 	// Use this for initialization
 	void Start () {
--- Assets/Scripts/shell.cs
+++ Assets/Scripts/shell.cs
@@ -5,18 +5,27 @@ public class shell : MonoBehaviour {
 	public GameObject sheelExplosion;
 	public float radius;
 	public float explosionForce;
+    public int attack;
 
 	void OnCollisionEnter () {
 		GameObject instant = Instantiate (sheelExplosion, transform.position, transform.rotation) as GameObject;
-		Collider[] colliders = Physics.OverlapSphere (transform.position, radius);
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
 		int l = colliders.Length;
-		for (int i = 0; i < l; i++) {
-			Rigidbody r = colliders [i].GetComponent<Rigidbody>();
-			if (r != null) {
-				r.AddExplosionForce (explosionForce, transform.position, radius);
-			}
-		}
-
+        Debug.Log(l);
+        for (int i = 0; i < l; i++)
+        {
+            Rigidbody r = colliders[i].GetComponent<Rigidbody>();
+            if (r != null)
+            {
+                r.AddExplosionForce(explosionForce, transform.position, radius);
+            }
+            unityClass tank = colliders[i].GetComponent<unityClass>();
+            if (tank != null)
+            {
+                Debug.Log("attack");
+                tank.Status(attack);
+            }
+        }
 		Destroy(gameObject);
 		Destroy (instant, (float)1.5 );
 	}
