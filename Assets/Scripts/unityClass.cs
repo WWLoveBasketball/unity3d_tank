4bfac546a524373e541a1a9473cb4e968c7815a5
wangwei <wangwweeii@sina.com>
2016-07-19 17:47:16 +0800

    no message

--- Assets/Scripts/unityClass.cs
+++ Assets/Scripts/unityClass.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class unityClass : MonoBehaviour {
 	public int Health = 100;
 	public GameObject ObjectDead;
+
 	public void Status (int attack) {
 		Health -= attack;
 		if (Health <= 0) {
