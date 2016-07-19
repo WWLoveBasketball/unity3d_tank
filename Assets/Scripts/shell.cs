44b0922d48f8144ab264ccf51e34e18d9e3dde13
wangwei <wangwweeii@sina.com>
2016-07-19 16:33:42 +0800

    add backgroundmusic&shell explosion

--- Assets/Scripts/shell.cs
+++ Assets/Scripts/shell.cs
@@ -3,9 +3,21 @@ using System.Collections;
 
 public class shell : MonoBehaviour {
 	public GameObject sheelExplosion;
+	public float radius;
+	public float explosionForce;
 
 	void OnCollisionEnter () {
-		Instantiate (sheelExplosion, transform.position, transform.rotation);
+		GameObject instant = Instantiate (sheelExplosion, transform.position, transform.rotation) as GameObject;
+		Collider[] colliders = Physics.OverlapSphere (transform.position, radius);
+		int l = colliders.Length;
+		for (int i = 0; i < l; i++) {
+			Rigidbody r = colliders [i].GetComponent<Rigidbody>();
+			if (r != null) {
+				r.AddExplosionForce (explosionForce, transform.position, radius);
+			}
+		}
+
 		Destroy(gameObject);
+		Destroy (instant, (float)1.5 );
 	}
 }
--- Assets/Scripts/tank.cs
+++ Assets/Scripts/tank.cs
@@ -6,6 +6,12 @@ public class tank : unityClass {
     public float movespeed, rotatespeed;
 	
 	void Update () {
+
+		float horizontal = Input.GetAxis ("Horizontal1");
+		float vertical = Input.GetAxis ("Vertical1");
+		transform.Translate (new Vector3 (0, 0, 1) * movespeed * Time.deltaTime * vertical);
+		transform.Rotate (new Vector3 (0, 1, 0) * rotatespeed * Time.deltaTime * horizontal);
+		/*
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(new Vector3(0, 0, 1) * movespeed * Time.deltaTime);
@@ -23,5 +29,6 @@ public class tank : unityClass {
         {
             transform.Rotate(new Vector3(0, 1, 0) * rotatespeed * Time.deltaTime);
         }
+        */
 	}
 }
